e2a2174e68cf1dcbab6308e68781d90eb1c1283b
sgoni <steven.goni@outlook.com>
2024-08-31 20:03:31 -0600

    Initial Commit of banking Microservice

---
+++ src/MicroRabbit.Infra.IoC/DepedencyContainer.cs
@@ -0,0 +1,13 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC;
+
+public class DepedencyContainer
+{
+    public static void RegisterServices(IServiceCollection services)
+    {
+        services.AddTransient<IEventBus, RabbitMQBus>();
+    }
+}
